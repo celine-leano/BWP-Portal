d29a0e14978173abd0e29535c2bf1db789499ebd
sammgabriel <smgabriel20@gmail.com>
2019-05-28 12:12:31 -0400

    Added a modal for physical assessment and a login for the API.

--- javascript/login-js.js
+++ javascript/login-js.js
@@ -5,21 +5,59 @@ Script for login page
  */
 let loginForm = document.getElementById('login-form');
 
-loginForm.onsubmit = function(e) {
+loginForm.onsubmit = function (e) {
     let email = document.getElementById('email-input').value;
     let password = document.getElementById('password-input').value;
     let loginErr = document.getElementById('loginErr');
-    let isValid = true;
+
+    $.ajax({
+        url: 'https://bwp-app.herokuapp.com/api/auth/rlogin',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          "username": email,
+          "password": password
+        }),
+        success: function (result) {
+            var token = result.token;
+            window.localStorage.setItem('token', token);
+            startSession();
+            return true;
+        },
+        error: displayLoginError()
+    });
+    
+    function displayLoginError()
+    {
+        loginErr.classList.remove('d-none');
+        e.preventDefault();
+        return false;
+    }
+
+    /**
+     * Starts session to verify that user is logged in and redirects to dashboard
+     */
+    function startSession()
+    {
+        $.ajax({
+            url: 'model/start-session.php',
+            type: 'POST',
+            success: function (result) {
+                location.href = "http://codedink.greenriverdev.com/BWP-Portal/dashboard";
+            }
+        })
+    }
 
     //display error if login is invalid
-    if (email !== "admin" || password !== "admin") {
-        loginErr.classList.remove('d-none');
-        isValid = false;
-    }
-
-    if (!isValid) {
-        e.preventDefault();
-    }
-
-    return isValid;
+    // if (email !== "admin" || password !== "admin") {
+    //     loginErr.classList.remove('d-none');
+    //     isValid = false;
+    // }
+    //
+    // if (!isValid) {
+    //     e.preventDefault();
+    // // }
+    //
+    // return isValid;
 };
